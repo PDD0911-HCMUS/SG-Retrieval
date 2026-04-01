ef94555dab567f2ad9947e64b844c9c3b18c5804
pdd0911 <dinhduytth1us@gmail.com>
2026-04-01 18:44:20 +0900

    commit change

--- config.py
+++ config.py
@@ -14,7 +14,8 @@ class ConfigDB:
     HOSTNAME = "localhost"
     DATABASE = "RetrievalSystemTraffic"
     USERNAME = "postgres"
-    PASSWORD = "123456"
+    # PASSWORD = "123456"
+    PASSWORD = "09111997"
     PORT = 5432 #5432 for Local #5433 for csMachine
     SQLALCHEMY_DATABASE_URI = f"postgresql://{USERNAME}:{PASSWORD}@{HOSTNAME}:{PORT}/{DATABASE}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- setup.sh
+++ setup.sh
@@ -10,4 +10,7 @@ echo $PYTHONPATH
 
 # CUDA_VISIBLE_DEVICES=0, python3 Controller/IRESGController/train.py 
 
-export JWT_SECRET_KEY="This_is_my_sceret_key"
+export JWT_SECRET_KEY="This_is_my_sceret_key" #linux
+
+setx JWT_SECRET_KEY "This_is_my_sceret_key" #win
+$env:JWT_SECRET_KEY="This_is_my_sceret_key" #win
